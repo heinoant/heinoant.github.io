e75b4dd69671ec116577102036392cb109ac01d4
heinoant <40440729+heinoant@users.noreply.github.com>
2024-07-02 13:43:19 +0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center text-2xl">
       <div className="w-2/5 font-medium">
-        i&apos;m a web developer based in turku, finland.
+        i&apos;m a web developer based in turku, finland
       </div>
     </main>
   );
