d196f1c7ff886616b58f60f493c70da0ea730800
heinoant <40440729+heinoant@users.noreply.github.com>
2024-07-02 13:21:37 +0300

    deploy testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/heinoant.github.io',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
+};
 
 export default nextConfig;
