7dc42506f032133d81a7a1c8845a803a271db0e4
heinoant <40440729+heinoant@users.noreply.github.com>
2024-11-18 15:48:54 +0200

    Layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={firaCode.className}>
-        <div className="noise"></div>
+        <div className="noise" />
+        <div className="vignette" />
         <div className="max-h-screen md:flex md:min-h-screen blur-[0.5px]">
-          <Navbar className="md:flex md:basis-1/3 md:justify-end pt-5 md:pt-0 pl-10 md:text-2xl text-end" />
+          <Navbar className="md:flex md:basis-1/3 md:justify-end pt-5 md:pt-0 pl-10 md:text-2xl text-end mb-10 md:mb-0" />
           {children}
         </div>
         <DarkmodeButton className="fixed text-2xl bottom-10 right-10 md:bottom-16 md:right-16 blur-[0.5px] transition ease-in-out hover:scale-[0.9] active:scale-[0.8] duration-200" />
